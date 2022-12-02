1d8e26f3f10a110916f553979e6021a691f4907c
danielasakpa <danielasakpa@gmail.com>
2022-12-02 20:20:06 +0100

    change browser router to hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import MainRouter from "./MainRouter";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import CartProvider from "./CartContext";
@@ -9,12 +9,12 @@ import CartProvider from "./CartContext";
 const App = () => {
   return (
     <CartProvider>
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <MainRouter />
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </CartProvider>
   );
 };
